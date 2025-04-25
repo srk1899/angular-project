c2843577686e480cd553af1db7d9e21959026df0
srk1899 <sunny.kashyap@bellblazetech.com>
2025-04-25 13:32:45 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "statefilebackup99" # Replace with your bucket name
+    bucket         = "ktt-test-frontend" # Replace with your bucket name
     key            = "terraform.tfstate"
     region         = "us-east-1"
   }
